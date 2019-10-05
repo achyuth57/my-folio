dbb82235d69ba8a4c1c61ba900f387c4ab2cd5e4
achyuth57 <achyuthkumar57@gmail.com>
2019-10-05 20:26:09 +0530

    Updates

---
+++ precache-manifest.ce4a4e30a0f6242b22827f25ac1e3534.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9a2015730bed593144e507d1212b1f",
+    "url": "/my-folio/index.html"
+  },
+  {
+    "revision": "5849ddc1e7db64e1adf1",
+    "url": "/my-folio/static/css/main.5aea8c38.chunk.css"
+  },
+  {
+    "revision": "916a283e04b09e488695",
+    "url": "/my-folio/static/js/2.6e1f04d0.chunk.js"
+  },
+  {
+    "revision": "5849ddc1e7db64e1adf1",
+    "url": "/my-folio/static/js/main.f7334a4e.chunk.js"
+  },
+  {
+    "revision": "56bea687738d65d9d416",
+    "url": "/my-folio/static/js/runtime~main.06803b53.js"
+  },
+  {
+    "revision": "06c8e05aeb458266ade1085896c0332f",
+    "url": "/my-folio/static/media/icomoon.06c8e05a.svg"
+  },
+  {
+    "revision": "8275504f5b9e186b54b8d84ebdedc2b4",
+    "url": "/my-folio/static/media/icomoon.8275504f.eot"
+  },
+  {
+    "revision": "dd6d51dddf740923fe9c94859aeee2bf",
+    "url": "/my-folio/static/media/icomoon.dd6d51dd.woff"
+  },
+  {
+    "revision": "ee8d016f1bfe9220bee450ff02b59624",
+    "url": "/my-folio/static/media/icomoon.ee8d016f.ttf"
+  }
+]);
