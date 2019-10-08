87a1fd1d97ba8c89ca9c0501a952a627aef6cc2b
achyuth57 <achyuthkumar57@gmail.com>
2019-10-08 13:01:46 +0530

    Commit  All Components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import HeaderComponent from "./components/HeaderComponent";
 import About from "./components/AboutComponent";
 import AppNavigation from "./components/AppNavigation";
 import ServicesComponent from "./components/ServicesComponent";
-
+import SkillsComponent from "./components/SkillsComponent";
+import EducationComponent from "./components/EducationComponent";
+import ExperienceComponent from "./components/ExperienceComponent";
+import WorkComponent from "./components/WorkComponent";
+import ContactComponent from "./components/ContactComponent";
 function App() {
   return (
     <div className="App" id="colorlib-page">
@@ -22,10 +26,12 @@ function App() {
               <Route exact path="/home" component={HeaderComponent} />
               <Route exact path="/about" component={About} />
               <Route exact path="/services" component={ServicesComponent} />
-              {/* <Route exactly component={Page1} pattern="/path1" />
-              <Route exactly component={Page2} pattern="/path2" />
-              <Route exactly component={Page3} pattern="/path3" />
-              <Route component={Page404} /> */}
+              <Route exact path="/skills" component={SkillsComponent} />
+              <Route exact path="/education" component={EducationComponent} />
+              <Route exact path="/experience" component={ExperienceComponent} />
+              <Route exact path="/work" component={WorkComponent} />
+              <Route exact path="/contact" component={ContactComponent} />
+              {/* <Route component={Page404} /> */}
             </Switch>
           </div>
         </Router>
